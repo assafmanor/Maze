8d387bc5b4242aaa6aa35e143330a22ca86b666d
Assaf Manor <assaf44@gmail.com>
2019-05-08 00:55:13 +0300

    algorithms removed from Match folder, AbstractAlgorithm class moved to Match folder

--- Match/AbstractAlgorithm.h
+++ Match/AbstractAlgorithm.h
@@ -1,12 +1,12 @@
 #pragma once
 
 class AbstractAlgorithm {
-public:
-	virtual ~AbstractAlgorithm() {}
-
-	enum Move { UP, RIGHT, DOWN, LEFT, BOOKMARK };
-
-	virtual Move move() = 0;
-	virtual void hitWall() = 0;
-	virtual void hitBookmark(int seq) = 0;
+  public:
+    virtual ~AbstractAlgorithm() {}
+ 
+    enum Move { UP, RIGHT, DOWN, LEFT, BOOKMARK };
+ 
+    virtual Move move() = 0;
+    virtual void hitWall() = 0;
+    virtual void hitBookmark(int seq) = 0;
 };
